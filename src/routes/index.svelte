39be15b7f0cf97ca7993af4ceb4826ff4b613378
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2019-10-16 17:17:41 +0200

    using lambda in sapper template

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,62 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 1rem auto;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+
+  div.button {
+    margin: 0 auto;
+    padding: 1rem 0;
+    width: 100%;
+    text-align: center;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Great success!</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="button">
+  <a
+    href="users"
+    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4
+    border border-gray-400 rounded shadow outline-none focus:outline-none">
+    Check users
+  </a>
+</div>
---
+++ src/routes/users.svelte
@@ -0,0 +1,75 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let emails = {};
+
+	onMount(async () => {
+    const emails = await fetch(`/.netlify/api/users`);
+    console.log(emails)
+	});
+</script>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+
+  div.button {
+    margin: 0 auto;
+    padding: 1rem 0;
+    width: 100%;
+    text-align: center;
+  }
+</style>
+
+<svelte:head>
+  <title>Users</title>
+</svelte:head>
+
+<div class="emails">
+	{#each emails as email}
+		<p>email</p>
+	{:else}
+		<!-- this block renders when photos.length === 0 -->
+		<p>loading...</p>
+	{/each}
+</div>
+
+<div class="button">
+  <a
+    href="/"
+    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4
+    border border-gray-400 rounded shadow outline-none focus:outline-none">
+    Check again
+  </a>
+</div>
