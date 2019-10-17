ae97c57c339911c87646377aba91a31eaee09a57
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2019-10-17 06:42:50 +0200

    Refactored as service app

--- lambda/users.js
+++ lambda/users.js
@@ -1,13 +1,22 @@
-const { processUsers } = require('../index')
-const { fetchUsers } = require('../lib/cognito')
-const { success, failure } = require('../lib/response')
+const { getUsers, addUsers } = require("../lib/google");
+const { fetchUsers } = require("../lib/cognito");
+const { success, failure } = require("../lib/response");
 
 export async function handler(event, context) {
   try {
-    const users = await fetchUsers()
-    processUsers(users)
-    return success({ msg: 'success' })
+    const users = await fetchUsers();
+    const emails = await getUsers();
+    if (users.length && emails.length && emails.length < users.length) {
+      let diff = users.filter(
+        user => !emails.includes(user.email.toLowerCase())
+      );
+      let data = diff.map(userdata => Object.values(userdata));
+      await addUsers(data);
+
+      return success(diff);
+    }
+    return success([])
   } catch (err) {
-    return failure(JSON.stringify(err))
+    return failure(err);
   }
 }
--- src/routes/users.svelte
+++ src/routes/users.svelte
@@ -1,54 +1,85 @@
 <script>
-	import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-	// let emails = [];
+  let loading = true;
+  let users = [];
 
-	onMount(async () => {
-    let emails = await fetch(`/.netlify/functions/users`);
-    console.log(emails)
-	});
+  onMount(async () => {
+    let res = await fetch(`/.netlify/functions/users`);
+    users = await res.json();
+    loading = false;
+  });
 </script>
 
 <style>
-  h1,
-  figure,
-  p {
+  h2,
+  h3 {
     text-align: center;
-    margin: 0 auto;
   }
 
-  h1 {
-    font-size: 2.8em;
+  h2 {
+    font-size: 1.75em;
     text-transform: uppercase;
-    font-weight: 700;
+    font-weight: 500;
     margin: 0 0 0.5em 0;
   }
 
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
+  h3 {
+    font-size: 1.25em;
+    text-transform: uppercase;
+    font-weight: 500;
+    padding: 1.5em 0;
   }
 
   @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+    h2 {
+      font-size: 2.5em;
     }
   }
 
-  div.button {
+  div.button,
+  div.emails {
     margin: 0 auto;
     padding: 1rem 0;
     width: 100%;
     text-align: center;
+  }
+
+  ol {
+    max-width: 80%;
+    text-align: left;
+    margin: 0 auto;
+  }
+
+  blockquote {
+    background: #eee;
+  }
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  div.spinner {
+    height: 200px;
+    position: relative;
+  }
+
+  .spinner:before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    width: 50px;
+    height: 50px;
+    margin-top: -10px;
+    margin-left: -10px;
+    border-radius: 50%;
+    border: 2px solid #ccc;
+    border-top-color: #000;
+    animation: spinner 0.6s linear infinite;
   }
 </style>
 
@@ -57,7 +88,29 @@
 </svelte:head>
 
 <div class="emails">
-
+  {#if loading}
+    <div class="spinner" />
+  {:else if users.length}
+    <h2>There are new users:</h2>
+    <ol class="list-decimal">
+      {#each users as cat}
+        <li>
+          <blockquote>
+            name: {cat.name}
+            <br />
+            phone_number: {cat.phone_number}
+            <br />
+            email: {cat.email}
+          </blockquote>
+        </li>
+      {/each}
+    </ol>
+    {#if users.length}
+      <h3>We have put them in the Google table</h3>
+    {/if}
+  {:else}
+    <h2>There are no new users...</h2>
+  {/if}
 </div>
 
 <div class="button">
@@ -65,6 +118,6 @@
     href="/"
     class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4
     border border-gray-400 rounded shadow outline-none focus:outline-none">
-    Check again
+    Done!
   </a>
 </div>
