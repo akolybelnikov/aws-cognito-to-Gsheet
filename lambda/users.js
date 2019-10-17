0f68750c0bc1561bd25bc52e37ff918c86a7ec2f
akolybelnikov <a.kolybelnikov@gmail.com>
2019-10-17 07:09:26 +0200

    Logging a response

--- lambda/users.js
+++ lambda/users.js
@@ -1,22 +1,24 @@
-const { getUsers, addUsers } = require("../lib/google");
-const { fetchUsers } = require("../lib/cognito");
-const { success, failure } = require("../lib/response");
+const { getUsers, addUsers } = require('../lib/google')
+const { fetchUsers } = require('../lib/cognito')
+const { success, failure } = require('../lib/response')
 
 export async function handler(event, context) {
   try {
-    const users = await fetchUsers();
-    const emails = await getUsers();
+    const users = await fetchUsers()
+    console.log(users)
+    const emails = await getUsers()
+    console.log(emails)
     if (users.length && emails.length && emails.length < users.length) {
       let diff = users.filter(
-        user => !emails.includes(user.email.toLowerCase())
-      );
-      let data = diff.map(userdata => Object.values(userdata));
-      await addUsers(data);
+        user => !emails.includes(user.email.toLowerCase()),
+      )
+      let data = diff.map(userdata => Object.values(userdata))
+      await addUsers(data)
 
-      return success(diff);
+      return success(diff)
     }
     return success([])
   } catch (err) {
-    return failure(err);
+    return failure(err)
   }
 }
